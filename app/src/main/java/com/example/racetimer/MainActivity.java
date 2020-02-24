0c7d463f8e25e59414e600dd738df37de0009444
Austin Begley <acbegley01@indianatech.net>
2020-02-24 12:44:48 -0500

    Added camera functionality

--- app/src/main/java/com/example/racetimer/MainActivity.java
+++ app/src/main/java/com/example/racetimer/MainActivity.java
@@ -20,6 +20,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.net.Uri;
+import android.view.View;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -66,4 +77,36 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+    private static int VIDEO_REQUEST =101;
+    private Uri videoUri = null;
+        public void captureVideo(View view)
+        {
+
+            Intent videoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+
+            if (videoIntent.resolveActivity(getPackageManager()) !=null )
+            {
+
+                startActivityForResult(videoIntent, VIDEO_REQUEST);
+            }
+
+        }
+
+        public void playVideo(View view)
+        {
+            Intent playIntent = new Intent(this,VideoPlayActivity.class);
+            playIntent.putExtra("videoUri",videoUri.toString());
+            startActivity(playIntent);
+        }
+
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+
+            if (requestCode==VIDEO_REQUEST && resultCode==RESULT_OK)
+            {
+
+                videoUri = data.getData();
+            }
+        }
 }
